c6771c0f530f92c695595242565e6f9a81fc2ac1
theo-js <dev.js.theo@gmail.com>
2021-10-24 23:59:24 +0200

    test: test configure method (callback params and calls)

--- __tests__/specs/configure.spec.ts
+++ __tests__/specs/configure.spec.ts
@@ -1,6 +1,13 @@
-var eardrum = require('../../dist/eardrum.js');
+/**
+ * @jest-environment jsdom
+ */
+const eardrum = require('../../dist/eardrum.js');
 
 describe('configure method helper', () => {
+	afterEach(() => {
+		eardrum.reset();
+	});
+
 	it('GIVEN property does not exist on object THEN should add property to object', () => {
 		const object = {} as any;
 		const property = 'MY_PROPERTY';
@@ -46,5 +53,106 @@ describe('configure method helper', () => {
 			value: null
 		});
 		expect(object2[property] === null).toBe(true);
+	});
+
+	it('GIVEN browser env THEN should call addEventListener on provided target with proper arguments', () => {
+		const object = {};
+		const property = 'MY_PROPERTY';
+		const value = 'MY_VALUE';
+		const target = document.createElement('div');
+
+		const handler = jest.fn();
+		const useCapture = true;
+		const type = 'click';
+		const addEventListenerSpy = jest.spyOn(target, 'addEventListener');
+		const providedListener = {
+			type,
+			target,
+			options: useCapture
+		};
+
+		eardrum.configure({
+			object, property, value,
+			handler,
+			listener: providedListener
+		});
+
+		expect(addEventListenerSpy).toHaveBeenCalledTimes(1);
+
+		// Trigger event on target
+		target.dispatchEvent(new Event(type));
+
+		const [receivedType, receivedHandler, receivedOptions] = addEventListenerSpy.mock.calls[0];
+
+		expect(addEventListenerSpy).toHaveBeenCalledTimes(1);
+		expect(receivedType).toEqual(type);
+		// We can't compare received handler because it is a function wrapping the original handler
+		expect(receivedOptions).toStrictEqual(useCapture);
+	});
+
+	it('GIVEN provided an event handler function THEN should be called and receive all required params when event is dispatched', () => {
+		const object = {};
+		const property = 'MY_PROPERTY';
+		const value = 'MY_VALUE';
+		const additionalRefProps = {};
+		const target = document.createElement('div');
+
+		let receivedEardrumConfigureArgs: any = null;
+		let receivedEvent: any = null;
+		const listenerRemovalCondition = (ref: any, index: number, array: any[]): boolean => false;
+
+		const handler = jest.fn(function (e: Event, eardrumConfigureArgs: Object): void {
+			receivedEvent = e;
+			receivedEardrumConfigureArgs = eardrumConfigureArgs;
+		});
+		const useCapture = true;
+		const type = 'click';
+		const providedListener = {
+			type,
+			target,
+			options: useCapture
+		};
+
+		eardrum.configure({
+			object, property, value,
+			handler,
+			listener: providedListener,
+			listenerRemovalCondition,
+			additionalRefProps
+		});
+
+		expect(handler).toHaveBeenCalledTimes(0);
+
+		// Trigger event on target
+		target.dispatchEvent(new Event(type));
+
+		expect(handler).toHaveBeenCalledTimes(1);
+		expect(receivedEvent instanceof Event).toBe(true);
+		expect(receivedEardrumConfigureArgs.object).toStrictEqual(object);
+		expect(receivedEardrumConfigureArgs.property === property).toBe(true);
+		expect(receivedEardrumConfigureArgs.listener).toStrictEqual(providedListener);
+		expect(receivedEardrumConfigureArgs.value === value).toBe(true);
+		expect(receivedEardrumConfigureArgs.handler).toStrictEqual(handler);
+		expect(receivedEardrumConfigureArgs.listenerRemovalCondition).toStrictEqual(listenerRemovalCondition);
+		expect(receivedEardrumConfigureArgs.additionalRefProps).toStrictEqual(additionalRefProps);
+		expect(receivedEardrumConfigureArgs.additionalRefProps).toStrictEqual(additionalRefProps);
+	});
+
+	it('GIVEN changed value of configured property THEN should use listenerRemovalCondition function', () => {
+		const property = 'MY_PROPERTY';
+		const object = { [property]: 'INITIAL_VALUE' };
+		const mockFn = jest.fn((ref: any, i: number, arr: any): any => console.log(arr));
+		eardrum.configure({
+			object,
+			property,
+			handler: jest.fn(),
+			listenerRemovalCondition: mockFn
+		});
+		expect(mockFn).not.toHaveBeenCalled();
+
+		// Set new value
+		object[property] = 'NEW_VALUE';
+
+		expect(mockFn).toHaveBeenCalled();
 	});
 });
